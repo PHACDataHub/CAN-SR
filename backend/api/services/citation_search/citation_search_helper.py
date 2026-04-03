35e255f2aa3dc977aa31c77590c9dd85d70a7ede
paopaoyan <paopaoyan2@gmail.com>
2026-04-03 15:18:30 +0000

    update search to use storage api

--- backend/api/services/citation_search/citation_search_helper.py
+++ backend/api/services/citation_search/citation_search_helper.py
@@ -1,6 +1,7 @@
 from typing import Dict, Any, List, Optional
 from ...core.security import get_current_active_user
 from ...core.config import settings
+from ..storage import storage_service
 
 import logging
 import os
@@ -9,28 +10,6 @@ from io import StringIO, BytesIO
 import azure.functions as func
 from azure.core.exceptions import AzureError
 from azure.storage.blob import BlobServiceClient
-def azure_client(container_name: str):
-    connection_string = settings.AZURE_STORAGE_CONNECTION_STRING
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_container_client(container_name)
-    return blob_client
-     
-
-def read_blob_to_df(blob_name: str, container_name: str) -> pd.DataFrame:
-    """Read a blob (Parquet or CSV) into a pandas DataFrame."""
-    blob_client = azure_client(container_name).get_blob_client(blob_name)
-    downloaded_blob = blob_client.download_blob().content_as_text()
-    blob_df = pd.read_csv(StringIO(downloaded_blob), sep=",")
-    return blob_df
-
-
-def write_df_to_blob(df: pd.DataFrame, blob_name: str, container_name: str):
-    """Write a pandas DataFrame to blob as Parquet."""
-    buffer = BytesIO()
-    df.to_csv(buffer, index=False, encoding='utf-8')
-    buffer.seek(0)
-    blob_client = azure_client(container_name).get_blob_client(blob_name)
-    blob_client.upload_blob(buffer, overwrite=True)
 
 def normalize_ids(df: pd.DataFrame):
     if "pmid" in df.columns:
@@ -41,18 +20,21 @@ def normalize_ids(df: pd.DataFrame):
         df["id"] = None
     return df
 
-def load_all_database_searches():
+async def load_all_database_searches():
     base_path = "citation-data/bronze-data"
 
     databases = ["Pubmed", "EuropePMC", "Scopus"]
     dfs = []
 
     for database in databases:
-        blob_name = f"{database}-all-citations.csv"
-        container = f"{base_path}/{database}"
+        all_path = (
+            f"{settings.STORAGE_CONTAINER_NAME}/"
+            f"{base_path}/{database}/{database}-all-citations.csv"
+        )
 
         try:
-            df = read_blob_to_df(blob_name, container)
+            bytes_data, _ = await storage_service.get_bytes_by_path(all_path)
+            df = pd.read_csv(BytesIO(bytes_data))
             df["source"] = database
             dfs.append(df)
         except Exception as e:
@@ -62,3 +44,9 @@ def load_all_database_searches():
         return pd.DataFrame()
 
     return pd.concat(dfs, ignore_index=True)
+
+async def save_df(path: str, df: pd.DataFrame):
+    buffer = BytesIO()
+    df.to_csv(buffer, index=False, encoding="utf-8")
+    buffer.seek(0)
+    await storage_service.put_bytes_by_path(path, buffer.getvalue(), "text/csv")
